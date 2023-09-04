45f4a4759e5fd8a6cbf4ee8526f8a5b950eac567
marcocentonze <marco.centonze27@gmail.com>
2023-09-04 16:40:15 +0200

    add variables,conditional and generate bombs function

--- campominato2/assets/script.js
+++ campominato2/assets/script.js
@@ -5,6 +5,38 @@ Al termine della partita il software deve comunicare il punteggio, cioè il nume
 
 // Imposta il limite di default fuori dalle funzioni
 let limit = 100;
+const fieldElement = document.querySelector('.field');
+let bombsArray = [];//array per memorizzare le posizioni delle bombe
+let stopGame = false;//variabile per capire se il gioco è over o continua
+
+// Definisci la funzione per generare la griglia
+function generateField(domElement, limit) {
+    // Genera la griglia in pagina
+    for (let i = 0; i < limit; i++) {
+        // Crea un elemento nel markup per rappresentare la singola cella
+        const cellElement = document.createElement("div");
+        // Aggiungi la classe "cell" e poi stampalo in pagina
+        cellElement.className = "cell";
+        cellElement.append(i + 1);
+        domElement.append(cellElement);
+        // Usa math.sqrt per calcolare la larghezza e .style.width per cambiarla nel CSS
+        cellElement.style.width = `calc(100% / ${Math.sqrt(limit)})`;
+        // Aggiungi un event listener quando si clicca sulla cella per cambiare il colore
+        cellElement.addEventListener('click', function () {
+            //if per verificare se il gioco è in corso
+            if (!stopGame) {
+                if (bombsArray.includes(Number(this.textContent))) 
+                {//Se si clicca su una bomba,casella diventa red e game over
+                    this.classList.add('bg-red');
+                    stopGame = true;
+                    alert("Hai perso!Mi dispiace ma hai cliccato su una bomba.");
+                } else {
+                    this.classList.toggle('bg-dark-blue'); //altrimenti il blu normale
+                }
+            }
+        });
+    }
+}
 
 // Aggiungi un evento change alla select
 document.getElementById('difficulty').addEventListener('change', function () {
@@ -21,35 +53,32 @@ document.getElementById('difficulty').addEventListener('change', function () {
     }
 });
 
-// Seleziona l'elemento field dalla DOM
-const fieldElement = document.querySelector('.field');
-
-// Aggiungi un evento al form per gestire il click sul pulsante "Start Game"
+// Aggiungi un evento al form per gestire il click sul pulsante "Avvia il gioco"
 document.querySelector('form').addEventListener('submit', function (ev) {
     ev.preventDefault();
 
     // Cancella il contenuto dell'elemento field
     fieldElement.innerHTML = '';
+    stopGame = false;
 
     // Genera la griglia utilizzando il limite corrente
     generateField(fieldElement, limit);
+
+    // Genera le posizioni delle bombe
+    bombsArray = generateBombs(limit);
 });
 
-// Definisci la funzione per generare la griglia
-function generateField(domElement, limit) {
-    // Genera la griglia in pagina
-    for (let i = 0; i < limit; i++) {
-        // Crea un elemento nel markup per rappresentare la singola cella
-        const cellElement = document.createElement("div");
-        // Aggiungi la classe "cell" e poi stampalo in pagina
-        cellElement.className = "cell";
-        cellElement.append(i + 1);
-        domElement.append(cellElement);
-        // Usa math.sqrt per calcolare la larghezza e .style.width per cambiarla nel CSS
-        cellElement.style.width = `calc(100% / ${Math.sqrt(limit)})`;
-        // Aggiungi un event listener quando si clicca sulla cella per cambiare il colore
-        cellElement.addEventListener('click', function () {
-            this.classList.toggle('bg-dark-blue'); // Alterna la classe 'bg-green' per cambiare il colore
-        });
+// Funzione per generare le posizioni delle bombe in modo casuale
+function generateBombs(limit) {
+    const bombPositions = [];
+    //ciclo while bombsposit.lenght<16...
+    while (bombPositions.length < 16) {
+        const bombPosition = Math.floor(Math.random() * limit) + 1;
+        if (!bombPositions.includes(bombPosition)) {
+            bombPositions.push(bombPosition);
+        }
     }
+    return bombPositions;
 }
+
+//stampare in pagina(creo primo il div su html)
